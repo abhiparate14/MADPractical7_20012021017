ad11fa73953097d7cc28b1f9dcfe7cbd96d785b6
Abhikumar Parate <abhiparate2523@gmail.com>
2022-10-01 16:35:18 +0530

    clock not working

--- app/src/main/java/com/example/madpractical7_20012021017/MainActivity.kt
+++ app/src/main/java/com/example/madpractical7_20012021017/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.madpractical7_20012021017
 
+import android.app.PendingIntent
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,3 +11,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+//private fun setAlarm(millisTime: Long, str: String)
+//{
+//    val intent = Intent(this, AlarmBroadcastReceiver::class.java)
+//    intent.putExtra("Service1", str)
+//    val pendingIntent = PendingIntent.getBroadcast(applicationContext)
+//}
